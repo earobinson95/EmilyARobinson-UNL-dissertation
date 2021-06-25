a1d5d49d2ebce8eb3b496b9a372cd4832a70d5cf
Emily Robinson <emily.robinson@huskers.unl.edu>
2021-06-25 12:40:34 -0500

    adjusted gamm model for youdrawit

--- analysis/youdrawit-eyefitting-model.R
+++ analysis/youdrawit-eyefitting-model.R
@@ -12,58 +12,91 @@ library(emmeans)
 library(pls)
 `%!in%` = function(x,y) !(x %in% y)
 source("analysis/gamm-predict-function.R")
-source("analysis/lmer-predict-function.R")
 
 # ------------------------------------------------------------------------------
 # IMPORT DATA ------------------------------------------------------------------
 # ------------------------------------------------------------------------------
+# FEEDBACK DATA
 feedback_smooth  <- read_csv("data/youdrawit-feedback-smooth.csv")
-
 eyefitting_data <- feedback_smooth %>%
   filter(parm_id %in% c("S", "F", "V", "N")) %>%
   select(participantID, nick_name, study_starttime, age, gender, academic_study, recruitment, plotID, start_time, end_time, parm_id, x, y, ydrawn, yloess, residualdrawn, residualloess) %>%
   arrange(participantID, x)
-
 factorCols = c('participantID', 'nick_name', 'age', 'gender', 'academic_study', 'recruitment', 'plotID', 'parm_id')
 eyefitting_data[,factorCols] <- lapply(eyefitting_data[,factorCols], factor)
-summary(eyefitting_data)
+# summary(eyefitting_data)
+
+# SIMULATED DATA
+simulated_data <- read_csv("data/youdrawit-simulated-data.csv") %>%
+  filter(study_starttime > 1620152231)  %>%
+  mutate(study_starttime = round(study_starttime)) %>%
+  mutate(participantID = md5(paste(nick_name, study_starttime)),
+         plotID = md5(paste(nick_name, study_starttime, parm_id)))
+
+factorCols = c('participantID', 'nick_name', 'plotID', 'parm_id', 'dataset')
+simulated_data[,factorCols] <- lapply(simulated_data[,factorCols], factor)
+# summary(simulated_data)
 
 # ------------------------------------------------------------------------------
 # OBTAIN FIRST PRINCIPAL COMPONENT ---------------------------------------------
 # ------------------------------------------------------------------------------
-# trial <- eyefitting_data %>%
-#   filter(participantID == "a47a513b05d2ae93a2778eefaae62831", parm_id == "F")
-# trial.pcr <- pcr(ydrawn ~ x, data = trial)
-# trial$ypcr <- predict(trial.pcr)
-# trial %>%
-#   ggplot(aes(x = x)) +
-#   geom_line(aes(y = y)) +
-#   geom_line(aes(y = ypcr), linetype = "dashed") +
-#   geom_line(aes(y = yloess), color = "steelblue") +
-#   facet_wrap(~parm_id) +
-#   theme_bw() +
-#   theme(aspect.ratio = 1)
 
-# Fit PCR
-pcrCalc <- function(data){
-  pcr.mod <- pcr(ydrawn ~ x, data = data)
-  predict(pcr.mod)
+# Trial Data
+participantIDs <- levels(eyefitting_data$participantID)
+trial.sim <- simulated_data %>%
+  filter(participantID == participantIDs[18], parm_id == "F", dataset == "point_data")
+summary(trial.sim)
+trial.feedback <- eyefitting_data %>%
+  filter(participantID == participantIDs[18], parm_id == "F")
+summary(trial.feedback)
+
+# PCA
+trial.pca <- prcomp(trial.sim[,c("x","y")])
+trial.sim$PC1 <- predict(trial.pca)[,1]
+trial.feedback$PC1 <- predict(trial.pca, newdata = trial.feedback)[,1]
+pca.lm <- lm(y ~ PC1, data = trial.sim)
+trial.feedback$ypca <- predict(pca.lm, newdata = trial.feedback)
+
+trial.feedback %>%
+  ggplot(aes(x = x)) +
+  geom_line(aes(y = y)) +
+  geom_line(aes(y = ypca), color = "black", linetype = "dashed") +
+  geom_line(aes(y = yloess), color = "steelblue") +
+  geom_point(data = trial.sim, aes(y = y)) +
+  facet_wrap(~parm_id) +
+  theme_bw() +
+  theme(aspect.ratio = 1) +
+  scale_x_continuous(limits = c(0,20))
+
+# Fit PCA
+pcaCalc <- function(data){
+  point.data <- simulated_data %>% 
+    mutate(participantID = as.character(participantID),
+           parm_id = as.character(parm_id)) %>%
+    filter(dataset == "point_data", participantID == as.character(data[1,"participantID"]), parm_id == as.character(data[1, 'parm_id']))
+  pca.mod <- prcomp(point.data[,c("x","y")])
+  point.data$PC1 <- predict(pca.mod)[,1]
+  data$PC1 <- predict(pca.mod, newdata = data)[,1]
+  pca.lm.mod <- lm(y ~ PC1, data = point.data)
+  predict(pca.lm.mod, newdata = data)
 }
 
-eyefitting_model_data <- eyefitting_data %>%
+eyefitting_model_data <- eyefitting_data %>% 
+  mutate(participantID = as.character(participantID),
+         parm_id = as.character(parm_id)) %>%
   tidyr::nest(-plotID) %>%
-  dplyr::mutate(ypcr = purrr::map(data, pcrCalc)) %>%
-  tidyr::unnest(cols = c(data, ypcr)) %>%
-  mutate(residualpcr = ydrawn - ypcr,
-         residualpcr.loess = yloess - ypcr) %>%
+  dplyr::mutate(ypca = purrr::map(data, pcaCalc)) %>%
+  tidyr::unnest(cols = c(data, ypca)) %>%
+  mutate(residualpca = ydrawn - ypca,
+         residualpca.loess = yloess - ypca,
+         participantID = factor(participantID),
+         parm_id = factor(parm_id)) %>%
   rename(yols = y,
          residualols = residualdrawn,
          residualols.loess = residualloess) %>%
-  dplyr::select(participantID, nick_name, study_starttime, age, gender, academic_study, recruitment, plotID, start_time, end_time, parm_id, x, yols, ypcr, ydrawn, yloess, residualols, residualols.loess, residualpcr, residualpcr.loess)
+  dplyr::select(participantID, nick_name, study_starttime, age, gender, academic_study, recruitment, plotID, start_time, end_time, parm_id, x, yols, ypca, ydrawn, yloess, residualols, residualols.loess, residualpca, residualpca.loess)
 
-head(eyefitting_model_data)
-
-# write.csv(feedback_smooth, file = "data/youdrawit-feedback-smooth.csv", row.names = F, na = "")
+head(eyefitting_model_data) 
 
 # ------------------------------------------------------------------------------
 # PLOT RAW DATA ----------------------------------------------------------------
@@ -74,35 +107,36 @@ eyefitting_model_data %>%
   ggplot(aes(x = x)) +
   geom_line(aes(y = yloess, group = plotID), alpha = 0.5, color = "steelblue") +
   geom_line(alpha = 0.2, aes(y = yols, group = participantID)) +
-  # geom_line(alpha = 0.2, aes(y = ypcr, group = participantID), linetype = "dashed") +
+  # geom_line(alpha = 0.2, aes(y = ypca, group = participantID), linetype = "dashed") +
   facet_wrap(~ parm_id) +
   theme_bw() +
   theme(aspect.ratio = 1) +
   scale_x_continuous(limits = c(0, 20)) +
   ggtitle("OLS")
 
-# yloess (PCR)
+# yloess (pca)
 eyefitting_model_data %>%
   ggplot(aes(x = x)) +
   geom_line(aes(y = yloess, group = plotID), alpha = 0.5, color = "steelblue") +
   # geom_line(alpha = 0.2, aes(y = yols, group = participantID)) +
-  geom_line(alpha = 0.2, aes(y = ypcr, group = participantID)) +
+  geom_line(alpha = 0.2, aes(y = ypca, group = participantID)) +
   facet_wrap(~ parm_id) +
   theme_bw() +
   theme(aspect.ratio = 1) +
   scale_x_continuous(limits = c(0, 20)) +
-  ggtitle("PCR")
+  ggtitle("PCA")
 
 # Residual Loess
 eyefitting_model_data %>%
   ggplot(aes(x = x)) +
-  geom_line(aes(y = residualols.loess, group = plotID), color = "steelblue", alpha = 0.5) +
-  geom_line(aes(y = residualpcr.loess, group = plotID), color = "orange", alpha = 0.5) +
+  geom_line(aes(y = residualols.loess, group = plotID, color = "OLS"), alpha = 0.3) +
+  geom_line(aes(y = residualpca.loess, group = plotID, color = "PCA"), alpha = 0.3) +
   geom_hline(yintercept = 0, linetype = "dashed") +
   facet_wrap(~parm_id) +
   theme_bw() +
   theme(aspect.ratio = 1) +
-  scale_x_continuous(limits = c(0, 20))
+  scale_x_continuous(limits = c(0, 20)) +
+  scale_color_manual("Estimate", values = c("steelblue", "orange"))
 
 # ------------------------------------------------------------------------------
 # FIT GAMM MODEL  --------------------------------------------------------------
@@ -111,28 +145,28 @@ eyefitting_model_data %>%
 # Fit GAMM
 
 # OLS
-tic()
+# tic()
 eyefitting.ols.gamm <- bam(residualols ~ -1 + parm_id + 
-              s(x, by = parm_id) +
-              s(participantID, bs = "re") +
-              s(x,participantID, bs = "re"),
-            method = "REML",
-            data = eyefitting_model_data)
-toc()
-summary(eyefitting.ols.gamm)
-anova(eyefitting.ols.gamm)
-
-# PCR
-tic()
-eyefitting.pcr.gamm <- bam(residualpcr ~ -1 + parm_id + 
                              s(x, by = parm_id) +
                              s(participantID, bs = "re") +
                              s(x,participantID, bs = "re"),
                            method = "REML",
                            data = eyefitting_model_data)
-toc()
-summary(eyefitting.pcr.gamm)
-anova(eyefitting.pcr.gamm)
+# toc()
+# summary(eyefitting.ols.gamm)
+# anova(eyefitting.ols.gamm)
+
+# pca
+# tic()
+eyefitting.pca.gamm <- bam(residualpca ~ -1 + parm_id + 
+                             s(x, by = parm_id) +
+                             s(participantID, bs = "re") +
+                             s(x,participantID, bs = "re"),
+                           method = "REML",
+                           data = eyefitting_model_data)
+# toc()
+# summary(eyefitting.pca.gamm)
+# anova(eyefitting.pca.gamm)
 
 # Obtain Predictions
 eyefitting.grid.gamm <- expand_grid(parm_id = c("S", "V", "F", "N"),
@@ -145,22 +179,22 @@ eyefitting.grid.gamm$ols.pred <- eyefitting.ols.preds$prediction
 eyefitting.grid.gamm$ols.lower <- eyefitting.ols.preds$prediction - (1.96 * eyefitting.ols.preds$se)
 eyefitting.grid.gamm$ols.upper <- eyefitting.ols.preds$prediction + (1.96 * eyefitting.ols.preds$se)
 
-# PCR
-eyefitting.pcr.preds <- predict_gamm(eyefitting.pcr.gamm, newdata = eyefitting.grid.gamm, se = T, re_form = NA)
-eyefitting.grid.gamm$pcr.pred <- eyefitting.pcr.preds$prediction
-eyefitting.grid.gamm$pcr.lower <- eyefitting.pcr.preds$prediction - (1.96 * eyefitting.pcr.preds$se)
-eyefitting.grid.gamm$pcr.upper <- eyefitting.pcr.preds$prediction + (1.96 * eyefitting.pcr.preds$se)
+# pca
+eyefitting.pca.preds <- predict_gamm(eyefitting.pca.gamm, newdata = eyefitting.grid.gamm, se = T, re_form = NA)
+eyefitting.grid.gamm$pca.pred <- eyefitting.pca.preds$prediction
+eyefitting.grid.gamm$pca.lower <- eyefitting.pca.preds$prediction - (1.96 * eyefitting.pca.preds$se)
+eyefitting.grid.gamm$pca.upper <- eyefitting.pca.preds$prediction + (1.96 * eyefitting.pca.preds$se)
 
 # Plot Predictions
 eyefitting.grid.gamm %>%
   filter((parm_id %in% c("F", "N", "S") | (x <= 16 & x >= 4))) %>%
   ggplot(aes(x = x)) +
-  geom_line(data = eyefitting_model_data, aes(x = x, y = residualpcr, group = plotID, color = "PCR"), alpha = 0.1) +
+  geom_line(data = eyefitting_model_data, aes(x = x, y = residualpca, group = plotID, color = "PCA"), alpha = 0.1) +
   geom_line(data = eyefitting_model_data, aes(x = x, y = residualols, group = plotID, color = "OLS"), alpha = 0.1) +
   geom_ribbon(aes(ymin = ols.lower, ymax = ols.upper, fill = "OLS"), color = NA, alpha = 0.7) +
   geom_line(aes(y = ols.pred, color = "OLS")) +
-  geom_ribbon(aes(ymin = pcr.lower, ymax = pcr.upper, fill = "PCR"), color = NA, alpha = 0.7) +
-  geom_line(aes(y = pcr.pred, color = "PCR")) +
+  geom_ribbon(aes(ymin = pca.lower, ymax = pca.upper, fill = "PCA"), color = NA, alpha = 0.7) +
+  geom_line(aes(y = pca.pred, color = "PCA")) +
   geom_hline(yintercept = 0, linetype = "dashed", alpha = 0.5) +
   facet_wrap(~parm_id) +
   theme_bw() +
@@ -177,46 +211,43 @@ eyefitting.grid.gamm %>%
 # Fit LMER
 
 # OLS
-tic()
+# tic()
 eyefitting.ols.lmer <- lmer(residualols ~ -1 + parm_id + x:parm_id + (1|participantID),
-                       data = eyefitting_model_data)
-toc()
-summary(eyefitting.ols.lmer)
-anova(eyefitting.ols.lmer)
-
-# PCR
-tic()
-eyefitting.pcr.lmer <- lmer(residualpcr ~ -1 + parm_id + x:parm_id + (1|participantID),
                             data = eyefitting_model_data)
-toc()
-summary(eyefitting.pcr.lmer)
-anova(eyefitting.pcr.lmer)
+# toc()
+# summary(eyefitting.ols.lmer)
+# anova(eyefitting.ols.lmer)
+
+# pca
+# tic()
+eyefitting.pca.lmer <- lmer(residualpca ~ -1 + parm_id + x:parm_id + (1|participantID),
+                            data = eyefitting_model_data)
+# toc()
+# summary(eyefitting.pca.lmer)
+# anova(eyefitting.pca.lmer)
 
 # Obtain Predictions
-eyefitting.grid.lmer <- expand_grid(parm_id = c("S", "V", "F", "N"),
-                         x = seq(0,20, 0.5),
-                         participantID = eyefitting_model_data$participantID[1])
+eyefitting.ols.grid.lmer  <- ref_grid(eyefitting.ols.lmer, at = list(x = seq(1,20,0.5)))
+eyefitting.ols.preds.lmer <- emmeans(eyefitting.ols.grid.lmer, ~ parm_id:x, cov.reduce = FALSE) %>% 
+  as_tibble()
 
-eyefitting.ols.preds <- predict_lmer(eyefitting.ols.lmer, newdata=eyefitting.grid.lmer, re.form = NA, se.fit = TRUE, nsim = 50)
-eyefitting.grid.lmer$ols.pred <- eyefitting.ols.preds$fit
-eyefitting.grid.lmer$ols.lower <- eyefitting.ols.preds$ci.fit[1,]
-eyefitting.grid.lmer$ols.upper <- eyefitting.ols.preds$ci.fit[2,]
+eyefitting.pca.grid.lmer  <- ref_grid(eyefitting.pca.lmer, at = list(x = seq(1,20,0.5)))
+eyefitting.pca.preds.lmer <- emmeans(eyefitting.pca.grid.lmer, ~ parm_id:x, cov.reduce = FALSE) %>% 
+  as_tibble()
 
-eyefitting.pcr.preds <- predict_lmer(eyefitting.pcr.lmer, newdata=eyefitting.grid.lmer, re.form = NA, se.fit = TRUE, nsim = 50)
-eyefitting.grid.lmer$pcr.pred <- eyefitting.pcr.preds$fit
-eyefitting.grid.lmer$pcr.lower <- eyefitting.pcr.preds$ci.fit[1,]
-eyefitting.grid.lmer$pcr.upper <- eyefitting.pcr.preds$ci.fit[2,]
+eyefitting.preds.lmer <- eyefitting.ols.preds.lmer %>%
+  full_join(eyefitting.pca.preds.lmer, by = c("x", "parm_id"), suffix = c(".ols", ".pca"))
 
 # Plot Predictions
-eyefitting.grid.lmer %>%
+eyefitting.preds.lmer %>%
   filter((parm_id %in% c("F", "N", "S") | (x <= 16 & x >= 4))) %>%
   ggplot(aes(x = x)) +
   geom_line(data = eyefitting_model_data, aes(x = x, y = residualols, group = plotID, color = "OLS"), alpha = 0.1) +
-  geom_line(data = eyefitting_model_data, aes(x = x, y = residualpcr, group = plotID, color = "PCR"), alpha = 0.1) +
-  geom_ribbon(aes(ymin = ols.lower, ymax = ols.upper, fill = "OLS"), color = NA, alpha = 0.7) +
-  geom_line(aes(y = ols.pred, color = "OLS")) +
-  geom_ribbon(aes(ymin = pcr.lower, ymax = pcr.upper, fill = "PCR"), color = NA, alpha = 0.7) +
-  geom_line(aes(y = pcr.pred, color = "PCR")) +
+  geom_line(data = eyefitting_model_data, aes(x = x, y = residualpca, group = plotID, color = "PCA"), alpha = 0.1) +
+  geom_ribbon(aes(ymin = asymp.LCL.ols, ymax = asymp.UCL.ols, fill = "OLS"), color = NA, alpha = 0.7) +
+  geom_line(aes(y = emmean.ols, color = "OLS")) +
+  geom_ribbon(aes(ymin = asymp.LCL.pca, ymax = asymp.UCL.pca, fill = "PCA"), color = NA, alpha = 0.7) +
+  geom_line(aes(y = emmean.pca, color = "PCA")) +
   geom_hline(yintercept = 0, linetype = "dashed", alpha = 0.5) +
   facet_wrap(~parm_id) +
   theme_bw() +
@@ -227,22 +258,22 @@ eyefitting.grid.lmer %>%
   ggtitle("Eyefitting LMER")
 
 # ------------------------------------------------------------------------------
-# Compare Sum of Squares (OLS VS PCR) ------------------------------------------
+# Compare Sum of Squares (OLS VS PCA) ------------------------------------------
 # ------------------------------------------------------------------------------
 
 ss_data <- eyefitting_model_data %>%
   group_by(participantID, nick_name, study_starttime, age, gender, academic_study, recruitment, plotID, parm_id, start_time, end_time) %>%
   summarise(olsSS = sum(residualols^2),
-            pcrSS = sum(residualpcr^2),
+            pcaSS = sum(residualpca^2),
             olsSS.loess = sum(residualols.loess^2),
-            pcrSS.loess = sum(residualpcr.loess^2)) %>%
+            pcaSS.loess = sum(residualpca.loess^2)) %>%
   ungroup() %>%
-  pivot_longer(cols = c("olsSS", "pcrSS", "olsSS.loess", "pcrSS.loess"),
+  pivot_longer(cols = c("olsSS", "pcaSS", "olsSS.loess", "pcaSS.loess"),
                names_to = "Fit",
                values_to = "SS")
 
 ss.lmer <- lmer(log(SS) ~ parm_id*Fit + (1|participantID),
-              data = ss_data %>% filter(Fit %in% c("olsSS", "pcrSS")))
+                data = ss_data %>% filter(Fit %in% c("olsSS", "pcaSS")))
 anova(ss.lmer)
 plot(ss.lmer)
 
@@ -257,7 +288,7 @@ ss.emmeans %>%
   geom_errorbar(aes(ymin = lower.CL, ymax = upper.CL, width = 0.5), position = position_dodge(0.9)) +
   theme_bw() +
   theme(aspect.ratio = 0.75) +
-  scale_fill_manual(values = c("steelblue", "orange"), labels = c("OLS", "PCR")) +
+  scale_fill_manual(values = c("steelblue", "orange"), labels = c("OLS", "PCA")) +
   scale_y_continuous("Sum of Squares") +
   scale_x_discrete("Data Set")
 
@@ -268,5 +299,5 @@ ss.slicediffs %>%
   geom_vline(xintercept = 1, linetype = "dashed") +
   theme_bw() +
   theme(aspect.ratio = 0.5) +
-  scale_x_continuous("Sum of Squares Odds Ratio \n (OLS vs PCR)", limits = c(0,10), breaks = seq(0,10,2)) +
+  scale_x_continuous("Sum of Squares Odds Ratio \n (OLS vs PCA)", limits = c(0,2), breaks = seq(0,2,0.5)) +
   scale_y_discrete("Data Set")
